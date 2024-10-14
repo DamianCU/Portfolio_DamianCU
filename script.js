b38bd40fc6e1f43dcef1e39a2fcae4e1985374ed
DamianCU <158050732+DamianCU@users.noreply.github.com>
2024-10-14 21:07:22 +0200

    ptf dcu

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ window.onscroll = () => {
         let top= window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute;
+        let id = sec.getAttribute('id');
     
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links =>{
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
         }
     })
 
 }
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
